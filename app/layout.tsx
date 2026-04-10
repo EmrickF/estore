b0548d6d6f9583bff0eb24bf2e5bf3d2ce98c06d
Emrick <erikfinnberg53@gmail.com>
2026-04-10 11:57:38 +0300

    Snälla funk bruh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-
-import  Navbar  from "@/components/ui/navbar"
+import "./globals.css"
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+import Navbar from "@/components/ui/navbar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
